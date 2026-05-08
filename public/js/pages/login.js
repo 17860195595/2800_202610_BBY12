faa12cf1de666b8ce933b39413029d5db66eea35
Adam Seh <adamsehtwo12@gmail.com>
2026-05-07 20:26:52 -0700

    end of sprint 2 -did js for login/signup both server side and front end side

--- public/js/pages/login.js
+++ public/js/pages/login.js
@@ -1,4 +1,36 @@
 /**
- * Login page entry (login.html).
- * Page-specific logic will go here.
+ * This just gets the inputs in the login fields when submit button
+ * is clicked and then sends them to the server via the req object.
+ * 
+ * @author Adam.S 
+ * @credit Looked at project from 1800 for reiview on how to do this
  */
+//adds event listner to submit button for click event
+document.getElementById('loginButtonID')
+    .addEventListener('click', async () => 
+    {
+        //input fields
+        const username = document.getElementById('loginUsernameID').value;
+        const password = document.getElementById('loginPasswordID').value;
+
+        //basically sets req object
+        const res = await fetch('/login', 
+        {
+            method: 'POST',
+
+            headers:
+            {
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify(
+            {
+                username,
+                password
+            })
+        });
+
+        //waits for servers reponse and alerts it on screen
+        const data = await res.json();
+        alert(data.message);
+    });
--- public/js/pages/signup.js
+++ public/js/pages/signup.js
@@ -1,4 +1,42 @@
 /**
- * Sign up page entry (signup.html).
- * Page-specific logic will go here.
+ * This just gets the inputs in the signup fields when submit button
+ * is clicked and then sends them to the server via the req object.
+ * 
+ * @author Adam.S 
+ * @credit Looked at project from 1800 for reiview on how to do this
  */
+document.getElementById('signupButtonID')
+    .addEventListener('click', async () => 
+    {
+        
+        const username = document.getElementById('signupUsernameID').value;
+
+        const password = document.getElementById('signupPasswordID').value;
+
+        //sends a req to server at /signup, server recives req
+        const res = await fetch('/signup', 
+        {
+            //req.method
+            method: 'POST',
+
+            //req.headers
+            headers: 
+            {   
+                //specifies json data being sent
+                'Content-Type': 'application/json'
+            },
+
+            //req.body
+            body: JSON.stringify(
+            {
+                username,
+                password
+            })
+        });
+
+        //reads servers request when its done and turn it into js object (client revcieves response res)
+        const data = await res.json();
+
+        //puts alert in page
+        alert(data.message);
+    });
